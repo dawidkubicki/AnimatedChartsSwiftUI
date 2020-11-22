320c4acc98918a4d063d871d7e784aa7185c2890
Dawid Kubicki <dawid@pluscode.io>
2020-11-22 12:57:22 +0100

    Initial Commit

---
+++ AnimatedChartsSwiftUI/AnimatedChartsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedChartsSwiftUIApp.swift
+//  AnimatedChartsSwiftUI
+//
+//  Created by Dawid Kubicki on 22/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedChartsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedChartsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatedChartsSwiftUI
+//
+//  Created by Dawid Kubicki on 22/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
