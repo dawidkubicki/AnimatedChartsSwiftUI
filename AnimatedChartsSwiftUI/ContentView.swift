2fc7197a915d1ac179f5b5ca16b3d50facedc35d
Dawid Kubicki <dawid@pluscode.io>
2020-11-22 15:22:23 +0100

    animation added

--- AnimatedChartsSwiftUI/ContentView.swift
+++ AnimatedChartsSwiftUI/ContentView.swift
@@ -11,10 +11,16 @@ struct ContentView: View {
     
     @State var pickerSelectedItem = 0
     
+    @State var dataPoints: [[CGFloat]] = [
+        [50,100,150,23,12],
+        [123,73,90,120,140],
+        [60,20,120,54,100]
+    ]
+    
     var body: some View {
         ZStack {
     
-            Color(#colorLiteral(red: 0, green: 0.8676494956, blue: 0.8651667237, alpha: 1)).edgesIgnoringSafeArea(.all)
+            Color("appBackground").edgesIgnoringSafeArea(.all)
         
             VStack {
                 Text("Calory Intake")
@@ -29,19 +35,13 @@ struct ContentView: View {
                 .padding(.horizontal, 24)
                 
                 HStack {
-                    VStack {
-                        ZStack (alignment: .bottom) {
-                            Capsule().frame(width: 30, height: 200)
-                                .foregroundColor(Color(#colorLiteral(red: 0.7156495452, green: 0.7113971114, blue: 0.7189193964, alpha: 0.7377255673)))
-                            Capsule().frame(width: 30, height: 150)
-                                .foregroundColor(.white)
-                        }
-                        Text("M")
-                            .fontWeight(.medium)
-                            .padding(.top, 8)
-                    
-                    }
+                    BarChartView(value: dataPoints[pickerSelectedItem][0])
+                    BarChartView(value: dataPoints[pickerSelectedItem][1])
+                    BarChartView(value: dataPoints[pickerSelectedItem][2])
+                    BarChartView(value: dataPoints[pickerSelectedItem][3])
+                    BarChartView(value: dataPoints[pickerSelectedItem][4])
                 }.padding(.top, 20)
+                .animation(.default)
             }
         }
     }
@@ -49,6 +49,30 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+struct BarChartView: View {
+    
+    var value: CGFloat = 0
+    
+    var body: some View {
+        VStack {
+            ZStack (alignment: .bottom) {
+                Capsule().frame(width: 30, height: 200)
+                    .foregroundColor(Color(#colorLiteral(red: 0.7156495452, green: 0.7113971114, blue: 0.7189193964, alpha: 0.7377255673)))
+                Capsule().frame(width: 30, height: value)
+                    .foregroundColor(.white)
+            }
+            Text("M")
+                .fontWeight(.medium)
+                .padding(.top, 8)
+            
+        }
     }
 }
